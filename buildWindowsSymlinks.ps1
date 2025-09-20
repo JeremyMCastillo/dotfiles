ba327bc38132fbd3b30d463ef9d3c3baf3077d9b
JeremyMCastillo <jcast029.jc@gmail.com>
2025-09-20 11:36:53 -0700

    Adding wezterm config and existing link cleanup

--- buildWindowsSymlinks.ps1
+++ buildWindowsSymlinks.ps1
@@ -1,2 +1,23 @@
 # Create symlinks for Windows Neovim configuration files
-mklink -Path "$env:USERPROFILE\AppData\Local\nvim" -Target "$PSScriptRoot\..\nvim" -ItemType Junction -Force
+
+function Clean-Existing-Link {
+    param (
+        [string]$LinkPath
+    )
+    if (Test-Path $LinkPath) {
+        Write-Host "Removing existing link or directory: $LinkPath"
+        Remove-Item $LinkPath -Recurse -Force
+    }
+}
+
+# Neovim conifg
+$Target = "$PSScriptRoot\nvim"
+$Link = "$env:USERPROFILE\AppData\Local\nvim"
+Clean-Existing-Link -LinkPath $Link
+cmd /c "mklink /J $Link $Target"
+
+# Wezterm config
+$Target = "$PSScriptRoot\.wezterm.lua"
+$Link = "$env:USERPROFILE\.wezterm.lua"
+Clean-Existing-Link -LinkPath $Link
+cmd /c "mklink /H $Link $Target"

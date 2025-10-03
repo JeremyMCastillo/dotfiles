37d1819a26e14245639ddf3da0197e92c636d56c
JeremyMCastillo <jcast029.jc@gmail.com>
2025-10-02 20:58:27 -0700

    Create delete all other buffers

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,17 +7,38 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map('i', '<C-l>', function()
-  vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
-end, {desc = 'Copilot Accept', noremap = true, silent = true})
+map("i", "<C-l>", function()
+  vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
+end, { desc = "Copilot Accept", noremap = true, silent = true })
 
-map('t', '<C-n>', '<C-\\><C-n>', { desc = "Terminal Normal Mode" })
+map("t", "<C-n>", "<C-\\><C-n>", { desc = "Terminal Normal Mode" })
 
 map("n", "<leader>cc", "<cmd>CopilotChatToggle<cr>", { desc = "Toggle Copilot Chat" })
 
 map("n", "<leader>lg", function()
-  vim.cmd("vsplit | terminal lazygit")
-  vim.cmd("startinsert")
+  vim.cmd "vsplit | terminal lazygit"
+  vim.cmd "startinsert"
 end, { desc = "LazyGit" })
+
+local function DeleteOtherBuffers()
+  local current_buf = vim.api.nvim_get_current_buf()
+  local current_tab = vim.api.nvim_get_current_tabpage()
+  local buffers = vim.api.nvim_list_bufs()
+  local tabs = vim.api.nvim_list_tabpages()
+
+  for _, buf in ipairs(buffers) do
+    if buf ~= current_buf and vim.api.nvim_buf_is_loaded(buf) then
+      vim.api.nvim_buf_delete(buf, { force = true })
+    end
+  end
+
+  for _, tab in ipairs(tabs) do
+    if tab ~= current_tab then
+      vim.api.nvim_tabpage_close(tab, true)
+    end
+  end
+end
+
+map("n", "<leader>wo", DeleteOtherBuffers, { desc = "Delete Other Buffers" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")

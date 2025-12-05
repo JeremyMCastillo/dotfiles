df307af77ec83b7da6ec707e7d5865925191007f
Jeremy Castillo <jeremy.castillo@tixtrack.com>
2025-12-05 11:02:19 -0800

    Fixing yamlls config

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -20,8 +20,6 @@ vim.lsp.config["yamlls"] = {
     yaml = {
       schemas = {
         ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-        ["../path/relative/to/file.yml"] = "/.github/workflows/*",
-        ["/path/from/root/of/project"] = "/.github/workflows/*",
       },
     },
   },
